eabe9b69b3593b44497aee635f13027c0429310e
aHassen23 <hassen.18@buckeyemail.osu.edu>
2023-02-20 18:31:03 -0500

    Contact route

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 const projectsRouter = require('./routes/projects')
+const contactsRouter = require('./routes/contact')
+
 
 app.set('view engine', 'ejs')
 app.use('/projects', projectsRouter)
+app.use('contact', contactsRouter)
 
 app.get('/' , (req,res) => {
     res.render('index')
